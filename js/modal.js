67c414ee8986bd745d525a29b26b0128b241595c
MaksymBachynskyi <125116688+MaksymBachynskyi@users.noreply.github.com>
2025-02-15 17:08:22 +0200

    q

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,33 @@
 		refs.modal.classList.toggle('is-hidden');
 	}
 })();
+const a = document.querySelector('.a');
+a.addEventListener('click', oncl);
+function oncl(e) {
+	console.log(e.currentTarget.elements);
+}
+const form = document.querySelector('.form-footer');
+form.addEventListener('submit', sub);
+
+function sub(ev) {
+	ev.preventDefault();
+	console.log(ev.currentTarget.elements.user_email.value);
+	const data = new FormData(ev.currentTarget);
+	console.log(data.get('user_email'));
+}
+// fetch('https://books-backend.p.goit.global/books/top-books')
+// 	.then(obj => obj.json())
+// 	.then(item =>
+// 		item.map(obj => a.insertAdjacentHTML('beforeend', createmarkup(obj)))
+// 	);
+
+// function createmarkup(arr) {
+// 	return `<div>${arr.list_name}<div>${createmarkupFor(b)}</div></div>`;
+// }
+// function createmarkupFor(arr) {
+// 	return arr
+// 		.map(item => {
+// 			return `<div>${item}</div>`;
+// 		})
+// 		.join('');
+// }
